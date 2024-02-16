3700a70a7d45e201609c56b301cc4564e2461bd4
kurumasora <skuruma@icloud.com>
2024-02-16 14:29:34 +0900

    add pro_controller

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 mod p9n_interface;
-mod ps5_dualsense;
-
-use drobo_interfaces::srv::{SolenoidStateSrv_Request, SolenoidStateSrv_Response};
+mod pro_controller;
+use drobo_interfaces::msg::MdLibMsg;
 use safe_drive::{
     context::Context,
     error::DynError,
@@ -9,27 +8,28 @@ use safe_drive::{
     msg::common_interfaces::{sensor_msgs, std_msgs},
     pr_fatal, pr_info,
     selector::Selector,
-    topic::{publisher::Publisher, subscriber::Subscriber},
+    topic::{publisher::{self, Publisher}, subscriber::Subscriber},
     RecvResult,
 };
 use std::{rc::Rc, time::Duration};
 
-pub mod DualsenseState {
-    pub const SQUARE: usize = 0;
-    pub const CIRCLE: usize = 1;
-    pub const TRIANGLE: usize = 2;
-    pub const CROSS: usize = 3;
-    pub const L1: usize = 4;
-    pub const L2: usize = 5;
-    pub const R1: usize = 6;
-    pub const R2: usize = 7;
+pub mod Switchstate {
+    pub const B: usize = 0;
+    pub const A: usize = 1;
+    pub const X: usize = 2;
+    pub const Y: usize = 3;
+    pub const SCROT: usize = 4;
+    pub const L: usize = 5;
+    pub const R: usize = 6;
+    pub const ZL: usize = 7;
+    pub const ZR: usize = 8;
     pub const D_PAD_UP: usize = 8;
-    pub const D_PAD_DOWN: usize = 9;
-    pub const D_PAD_LEFT: usize = 10;
-    pub const D_PAD_RIGHT: usize = 11;
-    pub const START: usize = 12;
-    pub const SELECT: usize = 13;
-    pub const PS: usize = 14;
+    //pub const D_PAD_DOWN: usize = 9;
+    //pub const D_PAD_LEFT: usize = 10;
+    //pub const D_PAD_RIGHT: usize = 11;
+    //pub const START: usize = 12;
+    //pub const SELECT: usize = 13;
+    //pub const PS: usize = 14;
 }
 
 fn main() -> Result<(), DynError> {
@@ -37,25 +37,24 @@ fn main() -> Result<(), DynError> {
     let node = ctx.create_node("controller_b", None, Default::default())?;
 
     let selector = ctx.create_selector()?;
-    let selector_client = ctx.create_selector()?;
     let subscriber = node.create_subscriber::<sensor_msgs::msg::Joy>("joy", None)?;
-
+    let publisher = node.create_publisher::<drobo_interfaces::msg::MdLibMsg>("md_driver_topic", None)?;
     
     worker(
         selector,
-        subscriber
-        pubrisher
+        subscriber,                               
+        publisher
     )?;
     Ok(())
 }
 
 fn worker(
     mut selector: Selector,
-    mut selector_client: Selector,
     subscriber: Subscriber<sensor_msgs::msg::Joy>,
+    publisher:Publisher<MdLibMsg>
     
 ) -> Result<(), DynError> {
-    let mut p9n = p9n_interface::PlaystationInterface::new(sensor_msgs::msg::Joy::new().unwrap());
+    let mut p9n = p9n_interface::NintendoSwitchInterface::new(sensor_msgs::msg::Joy::new().unwrap());
     let logger = Rc::new(Logger::new("controller_b"));
    
     
@@ -64,20 +63,23 @@ fn worker(
         Box::new(move |_msg| {
             p9n.set_joy_msg(_msg.get_owned().unwrap());
 
-            if p9n.pressed_r2(){
-                send_speed(0x04,true,50,0,&publisher)
-                send_speed(0x05,false,50,0,&publisher)
+            if p9n.pressed_zr(){
+                send_speed(0x04, 0, false, 50, 0,0,&publisher, /* publisher */);
+                send_speed(0x05, 0, true, 50, 0,0,&publisher, /* publisher */);
             }
-            if !p9n.pressed_r2(){
-                send_speed(0x04,true,0,0,&publisher)
-                send_speed(0x05,false,0,0,&publisher)
+            if !p9n.pressed_zr(){
+                send_speed(0x04, 0, false, 50, 0,0,&publisher, /* publisher */);
+                send_speed(0x05, 0, true, 50, 0,0,&publisher, /* publisher */);
+            }
+        
+            
+            if p9n.pressed_zr(){
+                send_speed(0x06, 0, true, 50, 90,0,&publisher, /* publisher */);
+            }
+            if !p9n.pressed_zr(){
+                send_speed(0x06, 0, true, 0, 90,0,&publisher, /* publisher */);
+            }
 
-            }
-            
-            if p9n.pressed_r1(){
-                send_speed(0x06,false,50,90,&publisher)
-            }
-            
             
             
            
@@ -88,7 +90,7 @@ fn worker(
     }
 
 }
-fn send_speed(_address:u32, _semi_id:u32,_phase:bool,_speed:u32,_angle:i32,publisher:&Publisher<MdLibMsg>){
+fn send_speed(_address:u32, _semi_id:u32,_phase:bool,_speed:u32,_angle:i32,_timeout:u16,publisher:&Publisher<MdLibMsg>){
     let mut msg = drobo_interfaces::msg::MdLibMsg::new().unwrap();
     msg.address = _address as u8;
     msg.semi_id = _semi_id as u8;
@@ -96,6 +98,21 @@ fn send_speed(_address:u32, _semi_id:u32,_phase:bool,_speed:u32,_angle:i32,publi
     msg.phase = _phase as bool;
     msg.power = _speed as u16;
     msg.angle = _angle as i32;
+    msg.timeout = _timeout as u16;
+
+    publisher.send(&msg).unwrap()
+
+}
+
+fn send_pwm(_address:u32, _semi_id:u32,_phase:bool,_power:u32,_angle:i32,_timeout:u16,publisher:&Publisher<MdLibMsg>){
+    let mut msg = drobo_interfaces::msg::MdLibMsg::new().unwrap();
+    msg.address = _address as u8;
+    msg.semi_id = _semi_id as u8;
+    msg.mode = 2 as u8; //MotorLibのPWMモードに倣いました
+    msg.phase = _phase as bool;
+    msg.power = _power as u16;
+    msg.angle = _angle as i32;
+    msg.timeout = _timeout as u16;
 
     publisher.send(&msg).unwrap()
 
--- src/p9n_interface.rs
+++ src/p9n_interface.rs
@@ -1,46 +1,40 @@
 use safe_drive::msg::common_interfaces::sensor_msgs;
-use crate::ps5_dualsense::AXES_DUALSENSE;
-use crate::ps5_dualsense::BUTTONS_DUALSENSE;
+use crate::pro_controller::AXES_SWITCH_PRO;
+use crate::pro_controller::BUTTONS_SWITCH_PRO;
 
-pub struct PlaystationInterface {
+pub struct NintendoSwitchInterface {
     msg: sensor_msgs::msg::Joy,
 }
 
-impl PlaystationInterface {
-    pub fn new(_msg: sensor_msgs::msg::Joy) -> PlaystationInterface {
-        PlaystationInterface { msg: _msg, }
+impl NintendoSwitchInterface {
+    pub fn new(_msg: sensor_msgs::msg::Joy) -> NintendoSwitchInterface {
+        NintendoSwitchInterface { msg: _msg, }
     }
     pub fn set_joy_msg(&mut self, _msg: sensor_msgs::msg::Joy){
         self.msg = _msg;
     }
     pub fn pressed_start(&self) -> bool {
-        self.msg.buttons.as_slice()[BUTTONS_DUALSENSE::START] == 1
+        self.msg.buttons.as_slice()[BUTTONS_SWITCH_PRO::PLUS] == 1
     }
-    pub fn pressed_l1(&self) -> bool {
-        self.msg.buttons.as_slice()[BUTTONS_DUALSENSE::L1] == 1
+    pub fn pressed_l(&self) -> bool {
+        self.msg.buttons.as_slice()[BUTTONS_SWITCH_PRO::L] == 1
     }
-    pub fn pressed_r1(&self) -> bool {
-        self.msg.buttons.as_slice()[BUTTONS_DUALSENSE::R1] == 1
+    pub fn pressed_r(&self) -> bool {
+        self.msg.buttons.as_slice()[BUTTONS_SWITCH_PRO::R] == 1
     }
-    pub fn pressed_l2(&self) -> bool {
-        self.msg.buttons.as_slice()[BUTTONS_DUALSENSE::L2] == 1
+    pub fn pressed_zl(&self) -> bool {
+        self.msg.buttons.as_slice()[BUTTONS_SWITCH_PRO::ZL] == 1
     }
-    pub fn pressed_r2(&self) -> bool {
-        self.msg.buttons.as_slice()[BUTTONS_DUALSENSE::R2] == 1
+    pub fn pressed_zr(&self) -> bool {
+        self.msg.buttons.as_slice()[BUTTONS_SWITCH_PRO::ZR] == 1
     }
     pub fn pressed_dpad_left(&self) -> bool {
-         self.msg.axes.as_slice()[AXES_DUALSENSE::DPAD_X] > 0.0
+         self.msg.axes.as_slice()[AXES_SWITCH_PRO::DPAD_X] > 0.0
     }
     pub fn pressed_dpad_up(&self) -> bool {
-        self.msg.axes.as_slice()[AXES_DUALSENSE::DPAD_Y] > 0.0
+        self.msg.axes.as_slice()[AXES_SWITCH_PRO::DPAD_Y] > 0.0
     }
     pub fn pressed_dpad_right(&self) -> bool {
-        self.msg.axes.as_slice()[AXES_DUALSENSE::DPAD_X] < 0.0
-    }
-    pub fn pressed_l2_analog(&self) -> f32 {
-        self.msg.axes.as_slice()[AXES_DUALSENSE::L2]
-    }
-    pub fn pressed_r2_analog(&self) -> f32 {
-        self.msg.axes.as_slice()[AXES_DUALSENSE::R2]
+        self.msg.axes.as_slice()[AXES_SWITCH_PRO::DPAD_X] < 0.0
     }
 }
